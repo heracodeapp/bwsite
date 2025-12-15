9ea1167fe32ddaf71f5bbfd41cb16e13534f3846
heracodeapp <heracodeapp@gmail.com>
2025-12-15 23:16:05 +0000

    Delete client directory

--- client/src/lib/schema-helpers.ts
+++
@@ -1,5 +0,0 @@
-import type { PaymentCode, Subscription } from "@shared/schema";
-
-export const getPaymentRef = (code: PaymentCode): string => code.stripePaymentId || "";
-export const getSubscriptionRef = (sub: Subscription): string => sub.stripeSubscriptionId || "";
-export const getCustomerRef = (sub: Subscription): string => sub.stripeCustomerId || "";
